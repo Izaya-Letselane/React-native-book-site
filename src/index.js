0048453fc1f46534a55d05f1c3d58028e202bedb
Izaya-Letselane <139796837+Izaya-Letselane@users.noreply.github.com>
2025-10-24 16:20:32 +0200

    cron.js added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import "dotenv/config"
+import job from './lib/cron.js'
+
 import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from './routes/bookRoutes.js'
 import {connectDB} from './lib/db.js'
@@ -8,6 +10,7 @@ import {connectDB} from './lib/db.js'
 const app = express()
 const PORT = process.env.PORT || 3000
 
+job.start()
 app.use(cors())
 app.use(express.json())//allows you pass json data
 
