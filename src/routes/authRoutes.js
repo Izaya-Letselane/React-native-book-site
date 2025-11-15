e7ef38920bea30c66cdd4e24117d0f547b33d177
Izaya-Letselane <139796837+Izaya-Letselane@users.noreply.github.com>
2025-11-15 23:01:42 +0200

    Add bookRoutes to backend3

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -53,7 +53,8 @@ router.post("/register", async (req, res)=>{
                 id: user._id,
                 username:user.username,
                 email:user.email,
-                profileImage:user.profileImage
+                profileImage:user.profileImage,
+                createdAt: user.createdAt,
             }
         })
 
@@ -89,7 +90,8 @@ router.post("/login", async (req, res)=>{
                     id:user._id,
                     username: user.username,
                     email:user.email,
-                    profileImage:user.profileImage
+                    profileImage:user.profileImage,
+                    createdAt: user.createdAt,
                 }
             })
             
